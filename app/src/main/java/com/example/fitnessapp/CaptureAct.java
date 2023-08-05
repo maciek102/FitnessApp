2d7b99ee9622cb9169aa8ad682b2d3d8d3e7a918
maciek102 <walczakmaciek@wp.pl>
2023-08-05 16:23:30 +0200

    scanner development

---
+++ app/src/main/java/com/example/fitnessapp/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.fitnessapp;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,25 +1,56 @@
 package com.example.fitnessapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
-import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView display;
+    private AppCompatButton btnScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        display = findViewById(R.id.text1);
-        new FetchProductInfoTask().execute("737628064502");
+        btnScan = findViewById(R.id.buttonScan);
+
+        /*display = findViewById(R.id.text1);
+        new FetchProductInfoTask().execute("737628064502");*/
     }
 
+    private void scanCode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    });
 
     //Performing network request asynchronously not to perform network operations on main thread, to avoid freezing the application
     private class FetchProductInfoTask extends AsyncTask<String, Void, String> {
